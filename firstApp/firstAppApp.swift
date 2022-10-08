f5779db2dcbee465a28e27621aa9b79f647da508
Hugo Montana <benzlbenny2k@gmail.com>
2022-10-08 18:05:49 +0300

    Initial commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by Hugo on 8.10.22.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
